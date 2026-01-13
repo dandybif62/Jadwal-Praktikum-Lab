65f2926c51c60badbf927bda6b567f30b52faf4a
Dandy <dandy7251@gmail.com>
2026-01-14 02:00:55 +0700

    pemodelan

---
+++ model.py
@@ -0,0 +1,16 @@
+class Mahasiswa:
+    def __init__(self, nim, nama):
+        self.nim = nim
+        self.nama = nama
+
+class Ruangan:
+    def __init__(self, nama, kapasitas):
+        self.nama = nama
+        self.kapasitas = kapasitas
+
+class Praktikum:
+    def __init__(self, nama, hari, mulai, selesai):
+        self.nama = nama
+        self.hari = hari
+        self.mulai = mulai
+        self.selesai = selesai
